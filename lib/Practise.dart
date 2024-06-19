28caa2a3c5a475febc07ac8107701fe127dcac05
Jenish Sigdel <148234112+IAMNOOBWWW@users.noreply.github.com>
2024-06-19 11:37:31 +0545

    Add files via upload

---
+++ lib/Practise.dart
@@ -0,0 +1,29 @@
+void main() {
+  print('welcome to dart');
+  var myC = myclass();
+  myC.printName("jhon cena"); // function calling
+  //
+  //
+  //
+  //
+  //
+  myC.printName("apply butter");
+  //
+  //
+  //
+  //
+  myC.printName("raaven");
+}
+
+class myclass {
+  int printName(String ne) {
+    //this function does not return anything.
+    print(ne); //defination of a function
+
+    int a, b;
+    a = 5;
+    b = 6;
+    int sum = a + b;
+    return sum;
+  }
+}
