a7b8ff673485fd76ed2fb83f35423467c345fa80
Jenish Sigdel <148234112+IAMNOOBWWW@users.noreply.github.com>
2024-06-23 13:25:40 +0545

    Add files via upload

---
+++ lib/maps and hashmaps.dart
@@ -0,0 +1,25 @@
+//maps in dart
+//-In Dart programming, maps are dictionary like data types that exist in keys-value form(known as lock-key).
+//-There is no restriction on the type of data that goes in a map data type.
+//-Maps are very flexible and can mutate their size based on the requirement.
+//-it is important to note that all locks(keys) need to be uniquely identify a map data type.
+
+//code to run map data type  //
+// main() {
+//   var map_name = {'key1': 'Value1', 'key2': 2, 'key3': 3.0, 'key4': true};
+
+//   print(map_name);
+// }
+
+//code to fetch any value
+// main() {
+//   var god_shiva = {'Key1': 'shivom', 'key2': 22, 'key3': 21.20, 'key4': false};
+//   print(god_shiva['key2']);
+// }
+
+//code to add more and extra values
+// main() {
+//   var lord_hatti = {'bot1': 'woooloo', 'bot2': 22, 'bot3': 2.09, 'bot4': true};
+//   lord_hatti['bot1'] = 'gogogogo';
+//   print(lord_hatti);
+// }
