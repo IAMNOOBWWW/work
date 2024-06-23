1d5ae7fcd7a5d8336aacebfdcee2da367b780eb5
Jenish Sigdel <148234112+IAMNOOBWWW@users.noreply.github.com>
2024-06-23 14:55:26 +0545

    Add files via upload

---
+++ lib/final_vs_const_key.dart
@@ -0,0 +1,11 @@
+// main() {
+//   print('godly');
+//   final name = "Shiva";//stays same throughout the program but can or cannot be inicilized at beginning at program.//
+//   //name="rudra"//if i write this then its not gonna work cuz the final name is declared as "shiva"
+
+// }
+
+// main() {
+//   print("gods");
+//   const name = "parwati"; // stays same throughout the program but must be inicilized at start of program.
+// }
